f4d4179d5ff3567a3232671b70eeaafa2949ac55
jan100br <jansenbruno@hotmail.com>
2023-02-16 00:54:56 -0300

    adicionandoum console log

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
 Console.log ("Rodando o sistema de gerenciamento de jogos")
+console.log("Aplicação está se conectando ao banco de dados")
+console.log("OPaulo fez esse commit sem você cer na aula !")
+console.log("Enviando para o Githunb")
+
