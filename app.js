4e97245f541cc5363279d2ba100c51ced4787a3d
jan100br <125107240+jan100br@users.noreply.github.com>
2023-02-16 00:58:52 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 Console.log ("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação está se conectando ao banco de dados")
-console.log("OPaulo fez esse commit sem você cer na aula !")
+console.log("O Paulo fez esse commit sem você ver na aula !")
 console.log("Enviando para o Githunb")
 
