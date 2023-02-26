b174bec907f12bccff4a87e2dd7bf4909d26ae4f
jan100br <125107240+jan100br@users.noreply.github.com>
2023-02-25 21:08:12 -0300

    corrigindo app js

--- app.js
+++ app.js
@@ -2,4 +2,3 @@ Console.log ("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação está se conectando ao banco de dados")
 console.log("O Paulo fez esse commit sem você ver na aula !")
 console.log("Enviando para o Githunb")
-
